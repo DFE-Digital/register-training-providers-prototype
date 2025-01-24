e61a479cd836e2325c57b996c45cf844f54c3408
Simon Whatley <simonwhatley@gmail.com>
2025-01-24 12:30:42 +0000

    Create stubs for users section

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,27 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 const userController = require('./controllers/users')
 
-router.get('/users', userController.index)
+const checkIsAuthenticated = (req, res, next) => {
+  next()
+}
+
+router.get('/users/new', checkIsAuthenticated, userController.newUser_get)
+router.post('/users/new', checkIsAuthenticated, userController.newUser_post)
+
+router.get('/users/new/check', checkIsAuthenticated, userController.newUserCheck_get)
+router.post('/users/new/check', checkIsAuthenticated, userController.newUserCheck_post)
+
+router.get('/users/:userId/edit', checkIsAuthenticated, userController.editUser_get)
+router.post('/users/:userId/edit', checkIsAuthenticated, userController.editUser_post)
+
+router.get('/users/:userId/edit/check', checkIsAuthenticated, userController.editUserCheck_get)
+router.post('/users/:userId/edit/check', checkIsAuthenticated, userController.editUserCheck_post)
+
+router.get('/users/:userId/delete', checkIsAuthenticated, userController.deleteUser_get)
+router.post('/users/:userId/delete', checkIsAuthenticated, userController.deleteUser_post)
+
+router.get('/users/:userId', checkIsAuthenticated, userController.userDetails)
+
+router.get('/users', checkIsAuthenticated, userController.usersList)
 
 module.exports = router
