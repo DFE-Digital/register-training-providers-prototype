893a7a2f6c9ba04bc67d40896e1f5299ca4590cd
Simon Whatley <simonwhatley@gmail.com>
2025-01-28 15:16:13 +0000

    Add provider routes

--- app/routes.js
+++ app/routes.js
@@ -15,6 +15,7 @@ router.use(flash())
 /// ------------------------------------------------------------------------ ///
 /// Controller modules
 /// ------------------------------------------------------------------------ ///
+const providerController = require('./controllers/providers')
 const userController = require('./controllers/users')
 
 /// ------------------------------------------------------------------------ ///
@@ -44,23 +45,45 @@ router.get('/', (req, res) => {
 /// ------------------------------------------------------------------------ ///
 /// USER ROUTES
 /// ------------------------------------------------------------------------ ///
-router.get('/users/new', checkIsAuthenticated, userController.newUser_get)
-router.post('/users/new', checkIsAuthenticated, userController.newUser_post)
+router.get('/users/new', checkIsAuthenticated, userController.newProvider_get)
+router.post('/users/new', checkIsAuthenticated, userController.newProvider_post)
 
-router.get('/users/new/check', checkIsAuthenticated, userController.newUserCheck_get)
+router.get('/users/new/check', checkIsAuthenticated, userController.newProviderCheck_get)
 router.post('/users/new/check', checkIsAuthenticated, userController.newUserCheck_post)
 
-router.get('/users/:userId/edit', checkIsAuthenticated, userController.editUser_get)
-router.post('/users/:userId/edit', checkIsAuthenticated, userController.editUser_post)
+router.get('/users/:userId/edit', checkIsAuthenticated, userController.editProvider_get)
+router.post('/users/:userId/edit', checkIsAuthenticated, userController.editProvider_post)
 
-router.get('/users/:userId/edit/check', checkIsAuthenticated, userController.editUserCheck_get)
+router.get('/users/:userId/edit/check', checkIsAuthenticated, userController.editProviderCheck_get)
 router.post('/users/:userId/edit/check', checkIsAuthenticated, userController.editUserCheck_post)
 
-router.get('/users/:userId/delete', checkIsAuthenticated, userController.deleteUser_get)
-router.post('/users/:userId/delete', checkIsAuthenticated, userController.deleteUser_post)
+router.get('/users/:userId/delete', checkIsAuthenticated, userController.deleteProvider_get)
+router.post('/users/:userId/delete', checkIsAuthenticated, userController.deleteProvider_post)
 
 router.get('/users/:userId', checkIsAuthenticated, userController.userDetails)
 
 router.get('/users', checkIsAuthenticated, userController.usersList)
+
+/// ------------------------------------------------------------------------ ///
+/// PROVIDER ROUTES
+/// ------------------------------------------------------------------------ ///
+router.get('/providers/new', checkIsAuthenticated, providerController.newProvider_get)
+router.post('/providers/new', checkIsAuthenticated, providerController.newProvider_post)
+
+router.get('/providers/new/check', checkIsAuthenticated, providerController.newProviderCheck_get)
+router.post('/providers/new/check', checkIsAuthenticated, providerController.newProviderCheck_post)
+
+router.get('/providers/:providerId/edit', checkIsAuthenticated, providerController.editProvider_get)
+router.post('/providers/:providerId/edit', checkIsAuthenticated, providerController.editProvider_post)
+
+router.get('/providers/:providerId/edit/check', checkIsAuthenticated, providerController.editProviderCheck_get)
+router.post('/providers/:providerId/edit/check', checkIsAuthenticated, providerController.editProviderCheck_post)
+
+router.get('/providers/:providerId/delete', checkIsAuthenticated, providerController.deleteProvider_get)
+router.post('/providers/:providerId/delete', checkIsAuthenticated, providerController.deleteProvider_post)
+
+router.get('/providers/:providerId', checkIsAuthenticated, providerController.providerDetails)
+
+router.get('/providers', checkIsAuthenticated, providerController.providersList)
 
 module.exports = router
