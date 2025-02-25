b1ddc0eaf08570d40a135c4bcc3bee554e14bc70
Simon Whatley <simonwhatley@gmail.com>
2025-02-25 14:36:51 +0000

    Implement filterToggleButton

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,25 @@
 // For guidance on how to add JavaScript see:
 // https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
 //
+import { FilterToggleButton } from '/public/javascripts/components/filter-toggle-button.js'
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
+  const filterToggleButton = new FilterToggleButton({
+    bigModeMediaQuery: '(min-width: 48.063em)',
+    startHidden: false,
+    toggleButton: {
+      container: document.querySelector('.app-action-bar__filter'),
+      showText: 'Show filter',
+      hideText: 'Hide filter',
+      classes: 'govuk-button--secondary govuk-!-margin-bottom-3'
+    },
+    closeButton: {
+      container: document.querySelector('.app-filter__header-action'),
+      text: 'Close'
+    },
+    filter: {
+      container: document.querySelector('.app-filter-layout__filter')
+    }
+  })
+  filterToggleButton.init()
 })
