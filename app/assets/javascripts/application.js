50ac662c5f9d0de9bc3e2f4895c3058e4e6ff172
Simon Whatley <simonwhatley@gmail.com>
2025-02-27 11:11:23 +0000

    Only run the filter toggle button code if page has filters

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,22 +5,29 @@
 import { FilterToggleButton } from '/public/javascripts/components/filter-toggle-button.js'
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  const filterToggleButton = new FilterToggleButton({
-    bigModeMediaQuery: '(min-width: 48.063em)',
-    startHidden: false,
-    toggleButton: {
-      container: document.querySelector('.app-action-bar__filter'),
-      showText: 'Show filter',
-      hideText: 'Hide filter',
-      classes: 'govuk-button--secondary govuk-!-margin-bottom-3'
-    },
-    closeButton: {
-      container: document.querySelector('.app-filter__header-action'),
-      text: 'Close'
-    },
-    filter: {
-      container: document.querySelector('.app-filter-layout__filter')
-    }
-  })
-  filterToggleButton.init()
+  const filterContainer = document.querySelector('.app-filter-layout__filter')
+  const actionBarFilterContainer = document.querySelector('.app-action-bar__filter')
+  const filterHeaderActionContainer = document.querySelector('.app-filter__header-action')
+
+  // Only create / init if filterContainer is present
+  if (filterContainer && actionBarFilterContainer && filterHeaderActionContainer) {
+    const filterToggleButton = new FilterToggleButton({
+      bigModeMediaQuery: '(min-width: 48.063em)',
+      startHidden: false,
+      toggleButton: {
+        container: actionBarFilterContainer,
+        showText: 'Show filter',
+        hideText: 'Hide filter',
+        classes: 'govuk-button--secondary govuk-!-margin-bottom-3'
+      },
+      closeButton: {
+        container: filterHeaderActionContainer,
+        text: 'Close'
+      },
+      filter: {
+        container: filterContainer
+      }
+    })
+    filterToggleButton.init()
+  }
 })
