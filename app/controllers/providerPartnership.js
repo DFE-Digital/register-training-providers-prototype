d28e44b4f8e5b0ca22dc9b28feea62587c82076f
Simon Whatley <simonwhatley@gmail.com>
2025-04-29 14:05:58 +0100

    Add check and view for duplicate partner

--- app/controllers/providerPartnership.js
+++ app/controllers/providerPartnership.js
@@ -1,7 +1,7 @@
-const { Op } = require('sequelize')
 const { Provider, ProviderPartnership } = require('../models')
 const Pagination = require('../helpers/pagination')
 const { isAccreditedProvider } = require('../helpers/accreditation')
+const { hasPartnership } = require('../helpers/partnership')
 
 /// ------------------------------------------------------------------------ ///
 /// List provider partnerships
@@ -185,8 +185,44 @@ exports.newProviderPartnership_post = async (req, res) => {
       }
     })
   } else {
-    res.redirect(`/providers/${providerId}/partnerships/new/check`)
+
+    let hasExistingPartnership
+    if (isAccredited) {
+      hasExistingPartnership = await hasPartnership({
+        accreditedProviderId: providerId,
+        trainingProviderId: req.session.data.provider.id
+      })
+    } else {
+      hasExistingPartnership = await hasPartnership({
+        accreditedProviderId: req.session.data.provider.id,
+        trainingProviderId: providerId
+      })
+    }
+
+    if (hasExistingPartnership) {
+      res.redirect(`/providers/${providerId}/partnerships/new/duplicate`)
+    } else {
+      res.redirect(`/providers/${providerId}/partnerships/new/check`)
+    }
   }
+}
+
+exports.newProviderPartnershipDuplicate_get = async (req, res) => {
+  // get the provider and partnership IDs from the request
+  const { providerId } = req.params
+  const provider = await Provider.findByPk(providerId)
+
+  const partner = await Provider.findByPk(req.session.data.provider.id)
+
+  res.render('providers/partnerships/duplicate', {
+    provider,
+    partner,
+    actions: {
+      back: `/providers/${providerId}/partnerships/new`,
+      cancel: `/providers/${providerId}/partnerships`,
+      change: `/providers/${providerId}/partnerships/new`
+    }
+  })
 }
 
 exports.newProviderPartnershipCheck_get = async (req, res) => {
