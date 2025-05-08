642a397fd8ddf1712b0ac003b328778184bc057a
Simon Whatley <simonwhatley@gmail.com>
2025-05-08 17:01:15 +0100

    Add provider_accreditation_revisions migration

---
+++ app/migrations/20250508161900-create-provider-accreditation-revision.js
@@ -0,0 +1,77 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('provider_accreditation_revisions', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      provider_accreditation_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'provider_accreditations',
+          key: 'id'
+        }
+      },
+      provider_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'providers',
+          key: 'id'
+        }
+      },
+      number: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      starts_on: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      ends_on: {
+        type: Sequelize.DATE
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      created_by_id: {
+        type: Sequelize.UUID,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_by_id: {
+        type: Sequelize.UUID,
+        allowNull: false
+      },
+      deleted_at: {
+        type: Sequelize.DATE
+      },
+      deleted_by_id: {
+        type: Sequelize.UUID
+      },
+      revision_number: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      revision_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      revision_by_id: {
+        type: Sequelize.UUID,
+        allowNull: true
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('provider_accreditation_revisions')
+  }
+}
