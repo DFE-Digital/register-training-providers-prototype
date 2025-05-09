ba65a64addd4c454b46ebbc297adf6061a0b32e0
Simon Whatley <simonwhatley@gmail.com>
2025-05-09 12:36:16 +0100

    Create userRevision model

---
+++ app/models/userRevision.js
@@ -0,0 +1,113 @@
+const { Model, DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+  class UserRevision extends Model {
+    static associate(models) {
+      UserRevision.belongsTo(models.User, {
+        foreignKey: 'userId',
+        as: 'user'
+      })
+
+      UserRevision.belongsTo(models.User, {
+        foreignKey: 'revisionById',
+        as: 'revisionByUser'
+      })
+    }
+  }
+
+  UserRevision.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: 'user_id'
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'first_name',
+        validate: {
+          notEmpty: true
+        }
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'last_name',
+        validate: {
+          notEmpty: true
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          isEmail: true
+        }
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: 'created_at'
+      },
+      createdById: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: 'created_by_id'
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: 'updated_at'
+      },
+      updatedById: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: 'updated_by_id'
+      },
+      archivedAt: {
+        type: DataTypes.DATE,
+        field: 'archived_at'
+      },
+      archivedById: {
+        type: DataTypes.UUID,
+        field: 'archived_by_id'
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        field: 'deleted_at'
+      },
+      deletedById: {
+        type: DataTypes.UUID,
+        field: 'deleted_by_id'
+      },
+      revisionNumber: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'revision_number'
+      },
+      revisionAt: {
+        type: DataTypes.DATE,
+        field: 'revision_at'
+      },
+      revisionById: {
+        type: DataTypes.UUID,
+        field: 'revision_by_id'
+      }
+    },
+    {
+      sequelize,
+      modelName: 'UserRevision',
+      tableName: 'user_revisions',
+      timestamps: false
+    }
+  )
+
+  return UserRevision
+}
