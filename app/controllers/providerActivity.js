c7d51dffb8c4a98813348d27dd62552dc87c8eb7
Simon Whatley <simonwhatley@gmail.com>
2025-05-13 14:37:12 +0100

    Get provider activity log

--- app/controllers/providerActivity.js
+++ app/controllers/providerActivity.js
@@ -1,4 +1,6 @@
 const { ActivityLog, Provider } = require('../models')
+const { getProviderActivityLogs } = require('../helpers/activityLog')
+const Pagination = require('../helpers/pagination')
 
 exports.activityList = async (req, res) => {
   // get the providerId from the request for use in subsequent queries
@@ -7,8 +9,24 @@ exports.activityList = async (req, res) => {
   // get the current provider
   const provider = await Provider.findByPk(providerId)
 
+  // variables for use in pagination
+  const page = parseInt(req.query.page, 10) || 1
+  const limit = parseInt(req.query.limit, 10) || 25
+  const offset = (page - 1) * limit
+
+  // Get the total amount of activity for pagination metadata
+  const totalCount = await ActivityLog.count()
+
+  const activityItems = await getProviderActivityLogs({ providerId, limit, offset })
+
+  // create the Pagination object
+  // using the chunk + the overall total count
+  const pagination = new Pagination(activityItems, totalCount, page, limit)
+
   res.render('providers/activity/index', {
     provider,
+    activityItems: pagination.getData(),
+    pagination,
     actions: {
 
     }
