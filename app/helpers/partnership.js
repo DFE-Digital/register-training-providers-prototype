6a7edf5d9aa38744bd52426888f732bcde00e3f1
Simon Whatley <simonwhatley@gmail.com>
2025-06-03 13:29:45 +0100

    Add getEligiblePartnerProviders helper function

--- app/helpers/partnership.js
+++ app/helpers/partnership.js
@@ -1,4 +1,6 @@
-const { ProviderPartnership } = require('../models')
+const { Provider, ProviderPartnership } = require('../models')
+
+const { Op } = require('sequelize')
 
 const hasPartnership = async (params) => {
   const partnershipCount = await ProviderPartnership.count({
@@ -11,6 +13,40 @@ const hasPartnership = async (params) => {
   return !!partnershipCount
 }
 
+const getEligiblePartnerProviders = async (query, today = new Date()) => {
+  const whereConditions = {
+    archivedAt: null,
+    deletedAt: null,
+    [Op.or]: [
+      { operatingName: { [Op.like]: `%${query}%` } },
+      { legalName: { [Op.like]: `%${query}%` } },
+      { ukprn: { [Op.like]: `%${query}%` } },
+      { urn: { [Op.like]: `%${query}%` } }
+    ]
+  }
+
+  return Provider.findAll({
+    attributes: ['id', 'operatingName', 'legalName', 'ukprn', 'urn'],
+    where: whereConditions,
+    include: [
+      {
+        model: ProviderAccreditation,
+        as: 'accreditations',
+        required: true,
+        where: {
+          startsOn: { [Op.lte]: today },
+          [Op.or]: [
+            { endsOn: null },
+            { endsOn: { [Op.gte]: today } }
+          ]
+        }
+      }
+    ],
+    order: [['operatingName', 'ASC']]
+  })
+}
+
 module.exports = {
-  hasPartnership
+  hasPartnership,
+  getEligiblePartnerProviders
 }
