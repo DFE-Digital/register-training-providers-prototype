5aabf3e0197308c1f5e1e8cb06655a78a87f27f2
Simon Whatley <simonwhatley@gmail.com>
2025-12-15 14:43:39 +0000

    Add API docs controller

---
+++ app/controllers/api/docs.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const fs = require('fs')
+const matter = require('gray-matter')
+
+const docsDirectory = path.join(__dirname, '../../views/api-docs/')
+
+const getDoc = (fileName) => {
+  const filePath = path.join(docsDirectory, `${fileName}.md`)
+  const doc = fs.readFileSync(filePath, 'utf8')
+  return matter(doc)
+}
+
+exports.providers = (req, res) => {
+  const markdown = getDoc('providers')
+
+  res.render('../views/api-docs/show', {
+    contentData: markdown.data,
+    content: markdown.content
+  })
+}
